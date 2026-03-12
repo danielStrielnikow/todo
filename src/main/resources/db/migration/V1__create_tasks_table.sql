066d74489d2b489a34f7ddb53848f6cad94d23ec
daniel.strielnikow <daniel.strielnikow@hrti.com.pl>
2026-03-12 19:46:31 +0100

    backend/feat: Add add dev and prod Spring profiles with H2 and PostgreSQL

--- src/main/resources/db/migration/V1__create_tasks_table.sql
+++ src/main/resources/db/migration/V1__create_tasks_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE tasks
 (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id UUID PRIMARY KEY,
     title       VARCHAR(255) NOT NULL,
     description VARCHAR(1000),
     status      VARCHAR(50)  NOT NULL DEFAULT 'NEW',
