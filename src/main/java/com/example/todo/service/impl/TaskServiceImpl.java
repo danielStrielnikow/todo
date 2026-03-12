3fe2061afa3895900ee25db94d450c855e1ef490
daniel.strielnikow <daniel.strielnikow@hrti.com.pl>
2026-03-12 23:00:33 +0100

    backend/feat: Create fast service crud task with exceptions

--- src/main/java/com/example/todo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/example/todo/service/impl/TaskServiceImpl.java
@@ -2,8 +2,13 @@ package com.example.todo.service.impl;
 
 import com.example.todo.dto.requestDto.TaskRequestDto;
 import com.example.todo.dto.responseDto.TaskResponseDto;
+import com.example.todo.exception.ResourceNotFoundException;
+import com.example.todo.mapper.TaskMapper;
+import com.example.todo.model.Task;
+import com.example.todo.model.enums.TaskStatus;
+import com.example.todo.repository.TaskRepository;
 import com.example.todo.service.TaskService;
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,34 +19,52 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class TaskServiceImpl implements TaskService {
     
+    private final TaskRepository taskRepository;
+    private final TaskMapper taskMapper;
+    
     
     @Override
     @Transactional
     public TaskResponseDto create(TaskRequestDto dto) {
-        return null;
+        Task task = taskMapper.toEntity(dto);
+        if (task.getStatus() == null) {
+            task.setStatus(TaskStatus.NEW);
+        }
+        return taskMapper.toResponse(taskRepository.save(task));
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<TaskResponseDto> findAll() {
-        return List.of();
+        return taskRepository.findAll()
+                .stream()
+                .map(taskMapper::toResponse)
+                .toList();
     }
 
     @Override
     @Transactional(readOnly = true)
     public TaskResponseDto findById(UUID id) {
-        return null;
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task not found with id: " + id));
+        return taskMapper.toResponse(task);
     }
 
     @Override
     @Transactional
     public TaskResponseDto update(UUID id, TaskRequestDto dto) {
-        return null;
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task not found with id: " + id));
+        taskMapper.updateEntity(dto, task);
+        return taskMapper.toResponse(taskRepository.save(task));
     }
 
     @Override
     @Transactional
     public void delete(UUID id) {
-
+        if (!taskRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Task not found with id: " + id);
+        }
+        taskRepository.deleteById(id);
     }
 }
