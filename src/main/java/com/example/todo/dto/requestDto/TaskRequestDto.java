88b110c32db0659d8476ece6429bd9958670e2e5
daniel.strielnikow <daniel.strielnikow@hrti.com.pl>
2026-03-12 19:53:56 +0100

    backend/feat: add reqyest and response dto and mapper

---
+++ src/main/java/com/example/todo/dto/requestDto/TaskRequestDto.java
@@ -0,0 +1,21 @@
+package com.example.todo.dto.requestDto;
+
+import com.example.todo.model.enums.TaskStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TaskRequestDto {
+
+    @NotBlank(message = "Title cannot be blank")
+    @Size(max = 255, message = "Title cannot exceed 255 characters")
+    private String title;
+
+    @Size(max = 1000, message = "Description cannot exceed 1000 characters")
+    private String description;
+
+    private TaskStatus status;
+}
